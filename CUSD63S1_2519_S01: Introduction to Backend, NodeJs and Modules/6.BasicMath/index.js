83013baa7c5bd351e8a5944577934fe67279839a
Dhruv garg <101688981+Dhruv-garg17@users.noreply.github.com>
2025-05-10 18:14:08 +0530

    Create index.js

---
+++ CUSD63S1_2519_S01: Introduction to Backend, NodeJs and Modules/6.BasicMath/index.js
@@ -0,0 +1,27 @@
+// index.js
+const sum = require("./sum");
+const multiply = require("./multiplication");
+const subtract = require("./subtraction");
+const divide = require("./division");
+
+const args = process.argv.slice(2);  // Skip first two default args
+const [operation, a, b] = args;
+const numA = Number(a);
+const numB = Number(b);
+
+switch (operation) {
+    case "sum":
+        console.log(sum(numA, numB));
+        break;
+    case "multiply":
+        console.log(multiply(numA, numB));
+        break;
+    case "subtract":
+        console.log(subtract(numA, numB));
+        break;
+    case "divide":
+        console.log(divide(numA, numB));
+        break;
+    default:
+        console.log("Unknown operation");
+}
